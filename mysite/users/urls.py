a505a0d6959fdabdb86f448ca934cb9cd2d56b70
chen <alf138540fun@gmail.com>
2024-08-15 12:58:26 +0200

    basic login function

---
+++ mysite/users/urls.py
@@ -0,0 +1,9 @@
+# users/views.py
+from django.urls import path
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path('login/', views.login_view, name='login')
+]
--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+
+
+def login_view(request):
+
+    if request.method == 'POST':
+        ##  获取表单中的数据
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        ##  与数据库中的用户名和密码比对
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse('Login succeed!')
+        else:
+            return HttpResponse('Login failed!')
+    return render(request, 'users/login.html')
