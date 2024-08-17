233457f546187cacf3be9e775f7a1338a324de5f
chen <alf138540fun@gmail.com>
2024-08-17 19:06:52 +0200

    blog models

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Article, Tag
+
+admin.site.register(Category)
+admin.site.register(Article)
+admin.site.register(Tag)
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=32, verbose_name='Category Name')
+    description = models.TextField(max_length=200, blank=True, default='', verbose_name='Category description')
+    add_date = models.DateTimeField(auto_now_add=True, verbose_name='Publish Time')
+    mod_date = models.DateTimeField(auto_now_add=True, verbose_name='Last Modified Time')
+
+    class Meta:
+        verbose_name = 'Blog Category'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=10, verbose_name='Article Tag')
+    add_date = models.DateTimeField(auto_now_add=True, verbose_name='Publish Time')
+    mod_date = models.DateTimeField(auto_now_add=True, verbose_name='Last Modified Time')
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=60, verbose_name='Article')
+    description = models.TextField(max_length=200, verbose_name='Article Description')
+    content = models.TextField(verbose_name='Article Content')
+    tags = models.ForeignKey(Tag, blank=True, null=True, on_delete=models.CASCADE, verbose_name='Article Tags')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Author')
+    add_date = models.DateTimeField(auto_now_add=True, verbose_name='Publish Time')
+    mod_date = models.DateTimeField(auto_now_add=True, verbose_name='Last Modified Time')
+
+    class Meta:
+        verbose_name = 'Article Label'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
