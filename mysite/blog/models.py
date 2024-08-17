1d3e4a2536ed527f0a5b5269ea7c2045b7d73e3b
chen <alf138540fun@gmail.com>
2024-08-17 19:28:10 +0200

    blog index access backend data

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Category(models.Model):
     name = models.CharField(max_length=32, verbose_name='Category Name')
     description = models.TextField(max_length=200, blank=True, default='', verbose_name='Category description')
@@ -31,7 +32,7 @@ class Article(models.Model):
     mod_date = models.DateTimeField(auto_now_add=True, verbose_name='Last Modified Time')
 
     class Meta:
-        verbose_name = 'Article Label'
+        verbose_name = 'Article'
         verbose_name_plural = verbose_name
 
     def __str__(self):
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from .models import Category, Article, Tag
 # Create your views here.
 
+
 def index(request):
-    return render(request, 'blog/index.html')
+    category_list = Category.objects.all()
+    article_list = Article.objects.all()
+    context = {'category_list': category_list, 'article_list': article_list}
+    return render(request, 'blog/index.html', context)
