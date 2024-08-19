24aba404107c211274458e94d9a530b618af73b4
chen <alf138540fun@gmail.com>
2024-08-20 01:50:01 +0200

    blog sidebar search function

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,5 +6,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('category/<int:category_id>', views.category_list, name='category_list'),
-    path('article/<int:article_id>', views.article_detail, name='article_detail')
+    path('article/<int:article_id>', views.article_detail, name='article_detail'),
+    path('search/', views.search, name='search'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Article, Tag
-# Create your views here.
+from django.db.models import Q, F
 
 
 def index(request):
@@ -24,10 +24,18 @@ def article_detail(request, article_id):
     prev_article = Article.objects.filter(id__lt=article_id).last()
     next_article = Article.objects.filter(id__gt=article_id).first()
 
-    # ordered by article publish date
-
-
-    print(prev_article, next_article)
-
     context = {'article': article, 'prev_article': prev_article, 'next_article': next_article}
     return render(request, 'blog/detail.html', context)
+
+
+def search(request):
+    keyword = request.GET.get('keyword')
+
+    if not keyword:
+        article_list = Article.objects.all()
+    else:
+        article_list = Article.objects.filter(Q(title__icontains=keyword) | Q(title__icontains=keyword) | Q(content__icontains=keyword))
+    context = {
+        'article_list': article_list
+    }
+    return render(request, 'blog/index.html', context)
