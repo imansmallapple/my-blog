5ed6b4f455718a4b9e680f52e1f295b223520170
chen <alf138540fun@gmail.com>
2024-09-08 17:44:45 +0200

    Added allowed host

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$*6=0tr!spyh@7m#34v9e%(nza852-cne3*xijr9%xij39s1v)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['51.20.54.201']
 
 
 # Application definition
